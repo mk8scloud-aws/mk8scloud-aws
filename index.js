cfa05344fe58c26640c5793f4ac272787ca676f6
mk8scloud-aws <mk8scloud@gmail.com>
2025-10-30 15:31:45 -0700

    Changed the Time Zone to PST

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let DATA = {
     hour: 'numeric',
     minute: 'numeric',
     timeZoneName: 'short',
-    timeZone: 'Europe/Stockholm',
+    timeZone: 'America/Los_Angeles',
   }),
 };/**
   * A - We open 'main.mustache'
